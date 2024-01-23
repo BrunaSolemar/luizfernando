e03408127b87358043f3cd677db4d9ecacd74890
Bruna Solemar <brunasolemar@gmail.com>
2024-01-22 23:28:41 -0300

    ex001

---
+++ exercicio001/ex01.js
@@ -0,0 +1,8 @@
+var idade = 16
+console.log(`A sia idade é ${idade} anos`);
+if (idade < 16){
+    (`Você ainda não tem idade para votar!`)
+} else 
+    (`Dirija-se a sua seção eleitoral.`)
+
+console.log(`Vote com consciência!`)
